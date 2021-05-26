11d899579cc6fb4819efa15790ff5cd389074bc9
Willian S <wsapcf@gmail.com>
2021-05-26 18:42:59 -0300

    add react navigation dependencies

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme' 
 import { Register } from './src/screens/Register';
 import { CategorySelect } from './src/screens/CategorySelect';
-
+import 'react-native-gesture-handler'
 export default function App() {
   const [fontsLoaded] = useFonts({Poppins_400Regular,Poppins_500Medium, Poppins_700Bold})
   if(!fontsLoaded)
