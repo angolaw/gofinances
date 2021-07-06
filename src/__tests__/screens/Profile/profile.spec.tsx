aba0b1c71c9fc032b35e3262366b5998267d306c
Willian S <wsapcf@gmail.com>
2021-07-06 11:47:18 -0300

    add textInput texts

--- src/__tests__/screens/Profile/profile.spec.tsx
+++ src/__tests__/screens/Profile/profile.spec.tsx
@@ -2,4 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import { Profile } from "../../../screens/Profile";
 
-test("should ", () => {});
+test("should render TextInput with correct placeholder - NAME ", () => {
+  const { getByPlaceholderText } = render(<Profile />);
+
+  const inputName = getByPlaceholderText("Nome");
+  // toBeTruthy - existir
+  expect(inputName.props.placeholder).toBeTruthy();
+});
+test("should render TextInput with correct placeholder - SURNAME ", () => {
+  const { getByPlaceholderText } = render(<Profile />);
+  const inputName = getByPlaceholderText("Sobrenome");
+  expect(inputName.props.placeholder).toBeTruthy();
+});
