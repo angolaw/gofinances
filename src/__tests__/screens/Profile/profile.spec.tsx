55bdfbd874f9d685863f20f023a815e2c17cb33e
Willian S <wsapcf@gmail.com>
2021-07-06 12:03:48 -0300

    add test suites and change tests name to it

--- src/__tests__/screens/Profile/profile.spec.tsx
+++ src/__tests__/screens/Profile/profile.spec.tsx
@@ -2,36 +2,38 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import { Profile } from "../../../screens/Profile";
 
-test("should render TextInput with correct placeholder - NAME ", () => {
-  const { getByPlaceholderText } = render(<Profile />);
+describe("Profile tests", () => {
+  it("should render TextInput with correct placeholder - NAME ", () => {
+    const { getByPlaceholderText } = render(<Profile />);
 
-  const inputName = getByPlaceholderText("Nome");
-  // toBeTruthy - existir
-  expect(inputName.props.placeholder).toBeTruthy();
-});
-test("should render TextInput with correct placeholder - SURNAME ", () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const inputName = getByPlaceholderText("Sobrenome");
-  expect(inputName.props.placeholder).toBeTruthy();
-});
-test("should render username and surname correctly", () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const username = getByPlaceholderText("Nome");
-  const surname = getByPlaceholderText("Sobrenome");
+    const inputName = getByPlaceholderText("Nome");
+    // toBeTruthy - existir
+    expect(inputName.props.placeholder).toBeTruthy();
+  });
+  it("should render TextInput with correct placeholder - SURNAME ", () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const inputName = getByPlaceholderText("Sobrenome");
+    expect(inputName.props.placeholder).toBeTruthy();
+  });
+  it("should render username and surname correctly", () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const username = getByPlaceholderText("Nome");
+    const surname = getByPlaceholderText("Sobrenome");
 
-  expect(username.props.value).toEqual("Willian");
-  expect(surname.props.value).toEqual("Santana");
-});
-test("should render button with correct testID", () => {
-  const { getByTestId } = render(<Profile />);
+    expect(username.props.value).toEqual("Willian");
+    expect(surname.props.value).toEqual("Santana");
+  });
+  it("should render button with correct testID", () => {
+    const { getByTestId } = render(<Profile />);
 
-  const butao = getByTestId("butao");
-  expect(butao).toBeTruthy();
-});
+    const butao = getByTestId("butao");
+    expect(butao).toBeTruthy();
+  });
 
-test("should render title correctly", () => {
-  const { getByTestId } = render(<Profile />);
+  it("should render title correctly", () => {
+    const { getByTestId } = render(<Profile />);
 
-  const profileTitle = getByTestId("profile-text");
-  expect(profileTitle.props.children).toContain("Perfil");
+    const profileTitle = getByTestId("profile-text");
+    expect(profileTitle.props.children).toContain("Perfil");
+  });
 });
