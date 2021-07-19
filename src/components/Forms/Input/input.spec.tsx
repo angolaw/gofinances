e4224ef7be9b336c09fff0a32bf6001486c8691f
Willian S <wsapcf@gmail.com>
2021-07-19 10:09:07 -0300

    add input test to match border color of input when active

--- src/components/Forms/Input/input.spec.tsx
+++ src/components/Forms/Input/input.spec.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
-
+import { Input } from ".";
 describe("input component", () => {
-  it("renders correctly", () => {
-    const { container } = render(<input />);
-    expect(container).toMatchSnapshot();
+  // must have specific border color when
+  it("should have specific border color", () => {
+    const { getByTestId } = render(
+      <Input
+        testID="input-email"
+        active={true}
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCorrect={false}
+      />
+    );
+    const inputComponent = getByTestId("input-email");
+    expect(inputComponent.props.style[0].borderColor).toEqual("#e83f5b");
   });
 });
