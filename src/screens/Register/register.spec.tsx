ef2c322423303327622abc905fdf6fcf4579bbdd
Willian S <wsapcf@gmail.com>
2021-07-19 12:13:19 -0300

    add waitFor clause for async actions

--- src/screens/Register/register.spec.tsx
+++ src/screens/Register/register.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Register } from ".";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "../../global/styles/theme";
 
@@ -9,10 +9,12 @@ const Providers: React.FC = ({ children }) => (
 );
 
 describe("Register Screen", () => {
-  it("should open category modal when user click category button", () => {
+  it("should open category modal when user click category button", async () => {
     const { getByTestId } = render(<Register />, { wrapper: Providers });
     const categoryModal = getByTestId("category-modal");
     const buttonCategory = getByTestId("button-category");
-    expect(categoryModal.props.visible).toBeTruthy();
+    // click on button using fireEvent
+    fireEvent.press(buttonCategory);
+    await waitFor(() => expect(categoryModal.props.visible).toBeTruthy());
   });
 });
