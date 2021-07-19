97cd178aec33c94a286ef44908c7f50cc65f137f
Willian S <wsapcf@gmail.com>
2021-07-19 11:45:17 -0300

    add register test and theme provider

---
+++ src/screens/Register/register.spec.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Register } from ".";
+import { render } from "@testing-library/react-native";
+
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../global/styles/theme";
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+describe("Register Screen", () => {
+  it("should open category modal when user click category button", () => {
+    const {} = render(<Register />);
+  });
+});
